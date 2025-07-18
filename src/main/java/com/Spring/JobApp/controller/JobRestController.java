19a7e07b05bd474177d2fa1e1fc63de9879d231f
Nilmoni Pangas <nilmonipangas17@gmail.com>
2025-07-18 16:34:30 +0530

    Added REST API with CRUD operations in JobRestController

---
+++ src/main/java/com/Spring/JobApp/controller/JobRestController.java
@@ -0,0 +1,58 @@
+package com.Spring.JobApp.controller;
+
+import com.Spring.JobApp.model.Job;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/jobs")
+public class JobRestController {
+    private List<Job> jobList = new ArrayList<>();
+
+    //get all jobs
+    @GetMapping
+    public List<Job> getAllJobs() {
+        return jobList;
+    }
+
+    //Get job by ID
+    @GetMapping("/{id}")
+    public Job getJobById(@PathVariable int id) {
+        return jobList.stream()
+                .filter(job -> job.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("job Not fount with id " + id));
+    }
+
+    //Post new job
+    @PostMapping
+    public String createNewJob(@RequestBody Job job) {
+        job.setId(jobList.size()+1);
+        jobList.add(job);
+        return "Job Created Successfully!";
+    }
+
+    //Deleting a job
+    @DeleteMapping("/{id}")
+    public String deleteJob(@PathVariable int id) {
+        boolean remove = jobList.removeIf(job -> job.getId() == id);
+        return remove ? "Job removed Successfully..." : "Job not found!!";
+    }
+
+    //Update job by id
+    @PutMapping("{id}")
+    public String updateJob(@PathVariable int id, @RequestBody Job updatedJob) {
+        for(Job job : jobList) {
+            if(job.getId() == id) {
+                job.setTitle(updatedJob.getTitle());
+                job.setCompany(updatedJob.getCompany());
+                job.setDescription(updatedJob.getDescription());
+                job.setLocation(updatedJob.getLocation());
+
+                return "Job updated successfully!";
+            }
+        }
+        return "Job not found!!";
+    }
+}
